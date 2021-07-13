c3798011c562ef7829a8c12311906108cbb3926f
aolivares182925 <182925@unsaac.edu.pe>
2021-07-13 12:51:33 -0500

    Inicio

---
+++ ProyectoGramaticas/ProyectoGramaticas/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace ProyectoGramaticas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelprograma1 = new System.Windows.Forms.Panel();
+            this.panelPrograma = new System.Windows.Forms.Panel();
+            this.txtRespuesta = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtReglas = new System.Windows.Forms.RichTextBox();
+            this.panelBotones1 = new System.Windows.Forms.Panel();
+            this.btnArreglarRecursividad = new System.Windows.Forms.Button();
+            this.btnArreglarAmbiguedad = new System.Windows.Forms.Button();
+            this.btnRyA = new System.Windows.Forms.Button();
+            this.btnAyR = new System.Windows.Forms.Button();
+            this.panelprograma1.SuspendLayout();
+            this.panelPrograma.SuspendLayout();
+            this.panelBotones1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelprograma1
+            // 
+            this.panelprograma1.Controls.Add(this.panelPrograma);
+            this.panelprograma1.Controls.Add(this.panelBotones1);
+            this.panelprograma1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelprograma1.Location = new System.Drawing.Point(0, 0);
+            this.panelprograma1.Name = "panelprograma1";
+            this.panelprograma1.Size = new System.Drawing.Size(1181, 548);
+            this.panelprograma1.TabIndex = 1;
+            // 
+            // panelPrograma
+            // 
+            this.panelPrograma.Controls.Add(this.txtRespuesta);
+            this.panelPrograma.Controls.Add(this.label6);
+            this.panelPrograma.Controls.Add(this.label5);
+            this.panelPrograma.Controls.Add(this.label2);
+            this.panelPrograma.Controls.Add(this.label1);
+            this.panelPrograma.Controls.Add(this.txtReglas);
+            this.panelPrograma.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelPrograma.Location = new System.Drawing.Point(200, 0);
+            this.panelPrograma.Name = "panelPrograma";
+            this.panelPrograma.Size = new System.Drawing.Size(981, 548);
+            this.panelPrograma.TabIndex = 1;
+            // 
+            // txtRespuesta
+            // 
+            this.txtRespuesta.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRespuesta.Location = new System.Drawing.Point(493, 157);
+            this.txtRespuesta.Name = "txtRespuesta";
+            this.txtRespuesta.Size = new System.Drawing.Size(450, 366);
+            this.txtRespuesta.TabIndex = 9;
+            this.txtRespuesta.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 83);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(378, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Debe de tener el sgu. formato: A -> b * i";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 45);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Reglas:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(324, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Solo se ingresa una regla por linea";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(201, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(315, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Arreglar ambiguedad y recursividad";
+            // 
+            // txtReglas
+            // 
+            this.txtReglas.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtReglas.Location = new System.Drawing.Point(6, 157);
+            this.txtReglas.Name = "txtReglas";
+            this.txtReglas.Size = new System.Drawing.Size(450, 366);
+            this.txtReglas.TabIndex = 0;
+            this.txtReglas.Text = "";
+            // 
+            // panelBotones1
+            // 
+            this.panelBotones1.Controls.Add(this.btnRyA);
+            this.panelBotones1.Controls.Add(this.btnAyR);
+            this.panelBotones1.Controls.Add(this.btnArreglarRecursividad);
+            this.panelBotones1.Controls.Add(this.btnArreglarAmbiguedad);
+            this.panelBotones1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelBotones1.Location = new System.Drawing.Point(0, 0);
+            this.panelBotones1.Name = "panelBotones1";
+            this.panelBotones1.Size = new System.Drawing.Size(200, 548);
+            this.panelBotones1.TabIndex = 0;
+            // 
+            // btnArreglarRecursividad
+            // 
+            this.btnArreglarRecursividad.Location = new System.Drawing.Point(12, 130);
+            this.btnArreglarRecursividad.Name = "btnArreglarRecursividad";
+            this.btnArreglarRecursividad.Size = new System.Drawing.Size(182, 55);
+            this.btnArreglarRecursividad.TabIndex = 1;
+            this.btnArreglarRecursividad.Text = "Recursividad";
+            this.btnArreglarRecursividad.UseVisualStyleBackColor = true;
+            this.btnArreglarRecursividad.Click += new System.EventHandler(this.btnArreglarRecursividad_Click);
+            // 
+            // btnArreglarAmbiguedad
+            // 
+            this.btnArreglarAmbiguedad.Location = new System.Drawing.Point(12, 65);
+            this.btnArreglarAmbiguedad.Name = "btnArreglarAmbiguedad";
+            this.btnArreglarAmbiguedad.Size = new System.Drawing.Size(182, 59);
+            this.btnArreglarAmbiguedad.TabIndex = 0;
+            this.btnArreglarAmbiguedad.Text = "Ambigüedad";
+            this.btnArreglarAmbiguedad.UseVisualStyleBackColor = true;
+            this.btnArreglarAmbiguedad.Click += new System.EventHandler(this.btnArreglarAmbiguedad_Click);
+            // 
+            // btnRyA
+            // 
+            this.btnRyA.Location = new System.Drawing.Point(12, 256);
+            this.btnRyA.Name = "btnRyA";
+            this.btnRyA.Size = new System.Drawing.Size(182, 55);
+            this.btnRyA.TabIndex = 3;
+            this.btnRyA.Text = "Recursividad y Ambigüedad";
+            this.btnRyA.UseVisualStyleBackColor = true;
+            this.btnRyA.Click += new System.EventHandler(this.btnRyA_Click);
+            // 
+            // btnAyR
+            // 
+            this.btnAyR.Location = new System.Drawing.Point(12, 191);
+            this.btnAyR.Name = "btnAyR";
+            this.btnAyR.Size = new System.Drawing.Size(182, 59);
+            this.btnAyR.TabIndex = 2;
+            this.btnAyR.Text = "Ambigüedad y  Recursividad";
+            this.btnAyR.UseVisualStyleBackColor = true;
+            this.btnAyR.Click += new System.EventHandler(this.btnAyR_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1181, 548);
+            this.Controls.Add(this.panelprograma1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelprograma1.ResumeLayout(false);
+            this.panelPrograma.ResumeLayout(false);
+            this.panelPrograma.PerformLayout();
+            this.panelBotones1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelprograma1;
+        private System.Windows.Forms.Panel panelPrograma;
+        private System.Windows.Forms.RichTextBox txtReglas;
+        private System.Windows.Forms.Panel panelBotones1;
+        private System.Windows.Forms.Button btnArreglarRecursividad;
+        private System.Windows.Forms.Button btnArreglarAmbiguedad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox txtRespuesta;
+        private System.Windows.Forms.Button btnRyA;
+        private System.Windows.Forms.Button btnAyR;
+    }
+}
+
---
+++ ProyectoGramaticas/ProyectoGramaticas/Form1.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoGramaticas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //obtener las reglas del txt
+        private void obtener(List<List<string>> A)
+        {
+            //dividir en lineas
+            string[] Campos = null;
+            Campos = txtReglas.Text.Split(new char[] { '\n' });
+            //campos = {{"regla 1"},{"regla 2"}...}
+            //oredenar
+            //Array.Sort(Campos);
+            //numero de reglas
+            int n = Campos.Length;
+            //crear lista de reglas(listas)
+            //List<List<string>> A = new List<List<string>>();
+            //añadir a listas
+
+            for (int i=0; i < n;i++)
+            {
+                List<string> cadena = new List<string>();
+                string[] aux = null;
+                aux = Campos[i].Split(' '); //aux = {"A","=","E","+"..}
+                cadena.Add(aux[0]);
+                cadena.Add(aux[2]);
+
+                string resto = "";
+                
+                for(int j=3;j < aux.Length;j++)
+                {
+                    if (j == aux.Length - 1)
+                        resto += aux[j];
+                    else
+                        resto += aux[j] + " ";
+                }
+                cadena.Add(resto);
+                A.Add(cadena);
+            }
+        }
+        // --------------------AMBIGÜEDAD------------------------------------
+        //funcion de existe ambiguedad
+        private List<Dictionary<object, object>> ExisteAmbiguedad(List<List<string>> A)
+        {
+            List<string> B = new List<string>();
+            for (int i = 0; i < A.Count; i++)
+            {
+                string s = A[i][0] + " " + A[i][1];
+                B.Add(s);
+            }
+            var b = new HashSet<string>(B).ToList();
+            Dictionary<object, object> amb = new Dictionary<object, object>
+            {
+            };
+            Dictionary<object, object> notamb = new Dictionary<object, object>
+            {
+            };
+            for (int i = 0; i < b.Count; i++)
+            {
+                List<object> e = new List<object>();
+                for (int j = 0; j < B.Count; j++)
+                {
+                    if (b[i] == B[j])
+                    {
+                        e.Add(j);
+                    }
+                }
+                if (e.Count > 1)
+                {
+                    amb[b[i]] = e;
+                }
+                else
+                {
+                    object[] Aa = b[i].Split();
+                    notamb[Aa[0]] = Aa[1];
+                }
+            }
+            var aux = new List<Dictionary<object, object>>();
+            aux.Add(amb);
+            aux.Add(notamb);
+            return aux;
+        }
+        //Funcion resolver ambiguedad
+        private void SolveAmbiguedad(List<List<string>> A, Dictionary<object, object> Amb, Dictionary<object, object> NotAmb)
+        {
+            string[] Aa;
+            foreach (KeyValuePair<object, object> i in Amb)
+            {
+                Aa = i.Key.ToString().Split();
+                List<string> S = new List<string> { Aa[0], Aa[1], Aa[0] + "a" };
+                A.Add(S);
+                if (!(NotAmb.ContainsKey(Aa[0])))
+                {
+                    List<string> vacio = new List<string> { Aa[0], "vacio", "" };
+                    A.Add(vacio);
+                }
+                foreach (int k in (List<object>)i.Value)
+                {
+                    List<string> Sol = new List<string> { Aa[0] + "a", A[k][2], "" };
+                    A[k] = Sol;
+                }
+            }
+        }
+
+        //------------------------------RECURSION--------------------------------------
+        // Funcion de existe recursion
+        private List<Dictionary<string, List<int>>> ExisteRecursion(List<List<string>> A)
+        {
+            List<Dictionary<string, List<int>>> aux = new List<Dictionary<string, List<int>>>();
+            Dictionary<string, List<int>> re = new Dictionary<string, List<int>>();
+            Dictionary<string, List<int>> notre = new Dictionary<string, List<int>>();
+            //Dictionary<string, int> notre = new Dictionary<string, int>();
+
+            for (int i = 0; i < A.Count; i++)
+            {
+                //si la  regla es recursiva
+                if (A[i][0] == A[i][1])
+                {
+                    if (re.ContainsKey(A[i][0]))
+                    {
+                        re[A[i][0]].Add(i);
+                    }
+                    else
+                    {
+                        List<int> list = new List<int>() { i };
+                        re.Add(A[i][0], list);
+                    }
+                }
+                //si la regla no es recursiva
+                else
+                {
+                    if (A[i][2] == "")
+                    {
+                        if (notre.ContainsKey(A[i][0]))
+                        {
+                            //notre[A[i][0]][0] = i;
+                            notre[A[i][0]].Add(i);
+                        }
+                        else
+                        {
+                            List<int> Elem = new List<int>() { i };
+                            notre.Add(A[i][0], Elem);
+                        }
+
+                    }
+                }
+
+            }
+            Dictionary<string, List<int>> aux2 = new Dictionary<string, List<int>>(notre);
+            //Dictionary<string, int> aux2 = new Dictionary<string, int>(notre);
+            //eliminar de aux2 aquellos simbolos de la izquierda que no tienen reglas recursivas 
+            foreach (var Comp in notre)
+            {
+                if (!re.ContainsKey(Comp.Key))
+                {
+                    aux2.Remove(Comp.Key);
+                }
+            }
+            aux.Add(re);
+            aux.Add(aux2);
+
+            return aux;
+        }
+        //Funcion solucion de recursion
+        private void SolRecursion(List<List<string>> A, Dictionary<string, List<int>> Re, Dictionary<string, List<int>> NotRe)
+        {
+            foreach (var elem in Re)
+            {
+                for (int j = 0; j < elem.Value.Count; j++)
+                {
+                    List<string> R = new List<string>()
+                    {
+                        elem.Key + "r", A[elem.Value[j]][2],elem.Key + "r"
+                    };
+                    A[elem.Value[j]] = R;
+                }
+
+            }
+            foreach (var val in NotRe)
+            {
+                for (int j =0; j < val.Value.Count;j++)
+                {
+                    List<string> S = new List<string>()
+                        {
+                            val.Key, A[val.Value[j]][1],val.Key + "r"
+                        };
+                    A[val.Value[j]] = S;
+                    //List<string> V = new List<string>()
+                    //    {
+                    //        val.Key + "r","vacio",""
+                    //    };
+                    //A.Add(V);
+                }
+                List<string> V = new List<string>()
+                    {
+                        val.Key + "r","vacio",""
+                    };
+                A.Add(V);
+            }
+        }
+
+        // Funcion para mostrar el resultado 
+        private string MostrarResultado(List<List<string>> A)
+        {
+            //variables
+            string Respuesta = "";
+            for (int i=0;i < A.Count;i++ )
+            {
+                Respuesta += "R" + i.ToString() + ": \t" + A[i][0].ToString() + " -> " + A[i][1] + " " + A[i][2] + "\n";
+            }
+
+            return Respuesta;
+        }
+        //--------------------------AMBIGÜEDAD----------------------------------------------
+        private string Ambiguedad(List<List<string>> A)
+        {
+            
+            //resolviendo la recursion
+            List<Dictionary<object, object>> Y = ExisteAmbiguedad(A);
+            //ExisteRecursion recursion en:
+
+            //obtener datos de llaves
+            Dictionary<object, object>.KeyCollection keys = Y[0].Keys;
+            string claves = "";
+            foreach (string llave in keys)
+            {
+                claves += llave + " ";
+            }
+            //obtener datos de variables
+            Dictionary<object, object>.ValueCollection values = Y[0].Values;
+            string valores = "";
+            foreach (List<object> lista in values)
+            {
+                int N = lista.Count;
+                string aux = "[ ";
+                foreach (int valor in lista)
+                {
+                    aux += valor.ToString() + " ";
+                }
+                aux += "]";
+                valores += aux;
+            }
+            string Respuesta = "Existe ambiguedad en: " + claves + " : " + valores + "\n" +
+                "-----------------------------\n";
+            SolveAmbiguedad(A, Y[0], Y[1]);
+            Respuesta += MostrarResultado(A);
+            return Respuesta;
+        }
+        //-------------------------RECURSIVIDAD------------------------------------------
+        private string Recursividad(List<List<string>> A)
+        {
+            //resolviendo la recursion
+            List<Dictionary<string, List<int>>> X = ExisteRecursion(A);
+            //ExisteRecursion recursion en:
+
+            //Mostrar 
+            //obtener datos de las llaves
+            Dictionary<string, List<int>>.KeyCollection keys = X[0].Keys;
+            string claves = "";
+            foreach (string llave in keys)
+            {
+                claves += llave + " ";
+            }
+            //obtener datos de variables
+            Dictionary<string, List<int>>.ValueCollection values = X[0].Values;
+            string valores = "";
+            foreach (List<int> lista in values)
+            {
+                int N = lista.Count;
+                string aux = "[ ";
+                foreach (int valor in lista)
+                {
+                    aux += valor.ToString() + " ";
+                }
+                aux += "]";
+                valores += aux;
+            }
+            string Respuesta = "Existe recursion en: " + claves + " : " + valores + "\n" +
+            //string Respuesta = "Existe recursividad en: " + X[0].Keys.ToString() + " : " + X[0].Values.ToString() + "\n" +
+                "-----------------------------\n";
+            SolRecursion(A, X[0], X[1]);
+
+            Respuesta += MostrarResultado(A);
+            return Respuesta;
+        }
+        //BOTONES
+        private void btnArreglarAmbiguedad_Click(object sender, EventArgs e)
+        {
+            //crear lista A
+            List<List<string>> A = new List<List<string>>();
+            obtener(A);
+            
+
+            txtRespuesta.Text = Ambiguedad(A);
+
+        }
+
+        private void btnArreglarRecursividad_Click(object sender, EventArgs e)
+        {
+            //crear lista A
+            List<List<string>> A = new List<List<string>>();
+            obtener(A);
+            
+
+            txtRespuesta.Text = Recursividad(A);
+        }
+
+        private void btnAyR_Click(object sender, EventArgs e)
+        {
+            //crear lista A
+            List<List<string>> A = new List<List<string>>();
+            obtener(A);
+            string Amb = Ambiguedad(A);
+            string Rec = Recursividad(A);
+
+            txtRespuesta.Text = Amb + "\n" + Rec;
+
+        }
+
+        private void btnRyA_Click(object sender, EventArgs e)
+        {
+            //crear lista A
+            List<List<string>> A = new List<List<string>>();
+            obtener(A);
+            string Rec = Recursividad(A);
+            string Amb = Ambiguedad(A);
+            
+            txtRespuesta.Text = Rec + "\n" + Amb;
+        }
+    }
+}
